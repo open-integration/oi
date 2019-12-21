b6f65862f05eee5267dc7fab3af66b1ee7102760
Oleg Sucharevich <olegs@codefresh.io>
2019-12-21 23:10:40 +0200

    add condition helper functions

--- condition.go
+++ condition.go
@@ -14,3 +14,15 @@ type (
 func ConditionEngineStarted(ev *Event, state *State) bool {
 	return ev.Metadata.Name == "engine.started"
 }
+
+// ConditionTaskFinishedWithStatus returns true once a task is finished with given status
+func ConditionTaskFinishedWithStatus(task string, status string) func(ev *Event, state *State) bool {
+	return func(ev *Event, state *State) bool {
+		for _, t := range state.Tasks {
+			if t.Status == status && t.State == "finished" && t.Task == task {
+				return true
+			}
+		}
+		return false
+	}
+}
