8b04689b96865ea8c46e8413edb5afdd12edbfac
Oleg Sucharevich <olegs@codefresh.io>
2019-12-22 15:58:00 +0200

    do not write by default to os.stdout

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -13,23 +13,18 @@ type (
 	}
 
 	Options struct {
-		Verbose  bool
 		FilePath string
-		Writer   io.Writer
 	}
 )
 
+// New creates new logger based on logger.Options
 func New(opt *Options) Logger {
 	l := log.New()
-	lvl := log.LvlDebug
 	handlers := []log.Handler{}
-	verboseHandler := log.LvlFilterHandler(lvl, log.StdoutHandler)
-	handlers = append(handlers, verboseHandler)
-
 	if opt != nil && opt.FilePath != "" {
 		h, err := log.FileHandler(opt.FilePath, log.LogfmtFormat())
 		if err == nil {
-			handlers = append(handlers, h)
+			handlers = append(handlers, log.LvlFilterHandler(log.LvlDebug, h))
 		}
 	}
 
@@ -37,12 +32,14 @@ func New(opt *Options) Logger {
 	return l
 }
 
+// NewFromFilePath creates new logger that writes to given file
 func NewFromFilePath(path string) Logger {
 	return New(&Options{
 		FilePath: path,
 	})
 }
 
+// NewWriter creates no io.WriteCloser , it is caller responsibility to close the writer
 func NewWriter(path string) (io.WriteCloser, error) {
 	file, err := os.Open(path)
 	return file, err
