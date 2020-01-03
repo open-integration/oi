ae0c2510364bf851bd6fad18881c73084e32b931
Oleg Sucharevich <olegs@codefresh.io>
2020-01-03 17:51:14 +0200

    update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,7 +12,10 @@ for pkg in `go list ./... | grep -v /vendor/`; do
     go test -v -covermode=atomic \
         -coverprofile=".cover/$(echo $pkg | sed 's/\//_/g').cover.out" $pkg
 done
+echo "mode: set" > .cover/cover.out && cat .cover/*.cover.out | grep -v mode: | sort -r | \
+   awk '{if($1 != last) {print $0;last=$1}}' >> .cover/cover.out
+
 code=$?
-go tool cover -html=cp.out -o coverage.html
+go tool cover -html=.cover/cover.out -o=.cover/coverage.html
 echo "go test cmd exited with code $code"
 exit $code
