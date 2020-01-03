96cd6485ef3bed6560869d59000bd60919e8ab91
Oleg Sucharevich <olegs@codefresh.io>
2020-01-03 17:41:00 +0200

    update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,17 @@
-go test -coverprofile cp.out ./pkg/modem
+
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+rm -rf .cover/ .test/
+mkdir .cover/ .test/
+trap "rm -rf .test/" EXIT
+
+for pkg in `go list ./... | grep -v /vendor/`; do
+    go test -v -covermode=atomic \
+        -coverprofile=".cover/$(echo $pkg | sed 's/\//_/g').cover.out" $pkg
+done
 code=$?
 go tool cover -html=cp.out -o coverage.html
 echo "go test cmd exited with code $code"
