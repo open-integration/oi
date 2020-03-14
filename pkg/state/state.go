5d83801fa6c57fb7d24882df3c25119c3170de14
Oleg <oleg2807@gmail.com>
2020-03-14 19:44:16 +0200

    save task timeline and draw in in graph

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -84,7 +84,7 @@ func (s *state) Copy() (State, error) {
 func (s *state) StateBytes() ([]byte, error) {
 	res := map[string]interface{}{
 		"metadata": map[string]interface{}{
-			"status": s.state,
+			"state": s.state,
 		},
 		"tasks":   s.tasks,
 		"service": s.services,
@@ -132,6 +132,7 @@ func (s *state) StartProcess() {
 				s.logger.Debug("Updating state", "request", "UpdateTaskStateRequest")
 				s.updateTaskStateRequest(updateRequest.UpdateTaskStateRequest)
 			}
+			s.wg.Done()
 		}
 	}
 }
@@ -198,7 +199,6 @@ func (s *state) updateTaskStateRequest(req *UpdateTaskStateRequest) {
 	s.tasks[task.Metadata.Name] = *newstate
 	s.events = append(s.events, *ev)
 	s.eventChan <- ev
-	s.wg.Done()
 }
 func (s *state) updateStateMetadataRequest(req *UpdateStateMetadataRequest) {
 	ev := &Event{
@@ -217,5 +217,4 @@ func (s *state) updateStateMetadataRequest(req *UpdateStateMetadataRequest) {
 	}
 	s.events = append(s.events, *ev)
 	s.eventChan <- ev
-	s.wg.Done()
 }
