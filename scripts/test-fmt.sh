3ca731072dadfa5511054e9aa8054d7da3b60252
Oleg Sucharevich <oleg2807@gmail.com>
2020-08-08 13:44:29 +0300

    update gofmt script

--- scripts/test-fmt.sh
+++ scripts/test-fmt.sh
@@ -3,9 +3,17 @@
 
 set -e
 
-fmtcmd="gofmt -l . | wc -l"
-if [ $(eval $fmtcmd) -gt 0 ]
-then
-    echo "cmd: \"$fmtcmd\" failed"
-    exit 1
-fi
+files=$(find . -type f -name '*.go')
+exitcode=0
+for f in $files
+do
+    cmd="gofmt -e -l $f | wc -l"
+    res=$(eval $cmd)
+    if [ $res -gt 0 ]
+    then
+        echo "cmd: \"$cmd\" failed. cmd result = $res"
+        exitcode=1
+    fi
+done
+
+exit $exitcode
