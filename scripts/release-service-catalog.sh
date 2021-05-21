683a9ababe5a9cc97845f9ccaf072110b643d33b
Oleg Sucharevich <oleg2807@gmail.com>
2021-05-21 20:37:07 +0300

    chore: update release catalog workflow

--- scripts/release-service-catalog.sh
+++ scripts/release-service-catalog.sh
@@ -2,6 +2,10 @@
 #!/bin/bash
 
 set -e
+FILES=$(ls -ls dist/catalog | awk 'NR>1' | awk '{print $10}')
+echo "Files to be uploaded:"
+echo "$FILES"
+
 echo $GOOGLE_SERVICE_ACCOUNT_B64 | base64 -d - > /tmp/service-account.json
 
 echo "Authentication with GCS project $GCS_PROJECT"
@@ -9,7 +13,4 @@ cat /tmp/service-account.json
 gcloud auth activate-service-account --key-file /tmp/service-account.json
 echo "Authenticated!"
 
-FILES=$(ls -ls dist/catalog | awk 'NR>1' | awk '{print $10}')
-echo "Files to be uploaded:"
-echo "$FILES"
 gsutil mv dist/catalog/* $GCS_BUCKET
