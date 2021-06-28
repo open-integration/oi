85f8042137ca2d8c5511c42723167373fdd52e40
Oleg Sucharevich <oleg2807@gmail.com>
2021-06-28 09:46:22 +0300

    fix: release binaries script

--- scripts/release-binaries.sh
+++ scripts/release-binaries.sh
@@ -3,13 +3,14 @@
 
 set -e
 
-SERVICES="http github slack"
+SERVICES="http github slack trello airtable"
 for s in $SERVICES
 do  
     V=$(cat catalog/services/$s/VERSION)
     NAME=$(echo $s | tr '[a-z]' '[A-Z]')
     FULL_NAME=$(echo "$NAME""_SERVICE_VERSION")
+    echo "Exporting $FULL_NAME=$V"
     export $FULL_NAME=$V
 done
 
-goreleaser release --rm-dist
+# goreleaser release --rm-dist
