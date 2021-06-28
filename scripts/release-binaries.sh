5301b78f8a791902ad11604549dceb8fa5c3d045
Oleg Sucharevich <oleg2807@gmail.com>
2021-06-28 06:48:29 +0300

    fix: release binaries script

--- scripts/release-binaries.sh
+++ scripts/release-binaries.sh
@@ -6,7 +6,7 @@ set -e
 SERVICES="http github slack"
 for s in $SERVICES
 do  
-    V=$(yq e '.version' catalog/services/$s/service.yaml)
+    V=$(cat catalog/services/$s/VERSION)
     NAME=$(echo $s | tr '[a-z]' '[A-Z]')
     FULL_NAME=$(echo "$NAME""_SERVICE_VERSION")
     export $FULL_NAME=$V
